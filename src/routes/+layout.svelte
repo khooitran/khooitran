67ddaa44d0b4cd0f37975643ae3428f62c0da506
khooi-lpt <khooi.tr@gmail.com>
2025-02-12 17:54:09 +0700

    working landing page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+  let { children } = $props();
+</script>
+
+<nav>
+  <a href="/">Home &gt;</a>
+  <a href="/projects">Projects &gt;</a>
+  <a href="/archive">Archive &gt;</a>
+  <a href="/about">About &gt;</a>
+  <a href="/contact">Contact &gt;</a>
+</nav>
+
+{@render children()}
+
+<style>
+  nav {
+    font-family: 'IBM Plex Mono', serif;
+    font-weight: 400;
+    font-style: normal;
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: #ffcc33;
+    text-decoration: underline;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Khoi Tran</h1>
+<p>
+  Hi! I am an aspiring creative developer and designer based in Hanoi, Vietnam. I have an interest
+  in typography and type design, both on paper and on the web. I am currently studying Design
+  Studies at RMIT University Hanoi.
+</p>
+
+<style>
+  h1,
+  p {
+    font-family: 'IBM Plex Mono', serif;
+  }
+
+  h1 {
+    font-weight: 900;
+  }
+
+  p {
+    font-weight: 400;
+  }
+</style>
