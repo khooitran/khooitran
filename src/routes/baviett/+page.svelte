d6f627f9f334f3eb68fc0c77537314d364b76bec
khooitran <khooi.tr@gmail.com>
2025-05-10 17:37:21 +0700

    added description for project pages

--- src/routes/baviett/+page.svelte
+++ src/routes/baviett/+page.svelte
@@ -4,5 +4,5 @@
 
 <HeroSection
   title="Baviett"
-  description="something"
+  description="This project was my first ever website project. The prompt was to create something that groups all the links to one place, and I guess I did just that. The project underwent a framework rework in February 2025 for me to learn more about Svelte and also optimising the workflow for future updates."
 />
--- src/routes/viettype/+page.svelte
+++ src/routes/viettype/+page.svelte
@@ -4,5 +4,6 @@
 
 <HeroSection
   title="ViệtType"
-  description="something"
+  description="This was a project to better understand typography and type design in Vietnam, for both myself and the public. Presented as a magazine, the project was a journey into the type design industry in Vietnam, featuring leading Vietnamese type designers."
+  endNote="&darr; scroll &darr;"
 />
